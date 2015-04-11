6a2e93a10b47ac436f87069085ad69525108feee
Sebastian Sanchez <sanchez.sebastian.r@gmail.com>
2015-04-10 23:35:43 -0300

    Ahora si se agrega settings con mysql

--- sovec/settings.py
+++ sovec/settings.py
@@ -73,11 +73,13 @@ WSGI_APPLICATION = 'sovec.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sovec_db',
+	'USER': 'root',
+	'PASSWORD': 'root',
+	'HOST': 'localhost',
     }
 }
 
