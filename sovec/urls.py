7d1f11503a0bec0e891ddbc7f392971d3ba29bdc
Sebastian Sanchez <sanchez.sebastian.r@gmail.com>
2015-04-11 22:50:29 -0300

    Se agrega ejemplo de autentificacion, con 2 paginas home (1 para logeado y otra pa no logeados). Se agrega jquery file. Se agrega logout

--- sovec/urls.py
+++ sovec/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from venta_entradas.views import hello
+from venta_entradas.views import home
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'sovec.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^hello/$', hello),
+    url(r'^$', home),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+                          {'next_page': '/'}),
 ]
--- venta_entradas/views.py
+++ venta_entradas/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
-def hello(request):
-    return render_to_response('index.html')
-# Create your views here.
+def home(request):
+	if request.user.is_authenticated():
+		return render_to_response('home.html',{'auth':True})
+	else:
+		return render_to_response('home.html',{'auth':False})
+
