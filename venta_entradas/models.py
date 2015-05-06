71887ec7c3f56449e4008c0bdd8f65314addfb91
Brahim <busenmo@gmail.com>
2015-05-06 01:20:36 -0300

    Base de datos agregada

--- venta_entradas/models.py
+++ venta_entradas/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pelicula(models.Model):
+	idPelicula = models.AutoField(primary_key=True)
+	nombre = models.CharField(max_length=100)
+	descripcion = models.CharField(max_length=1000)
+	duracion = models.DateTimeField()
+
+class Sala(models.Model):
+	idSala = models.AutoField(primary_key=True)
+	numero = models.IntegerField()
+
+class Asiento(models.Model):
+	idAsiento = models.AutoField(primary_key=True)
+	idSala = models.ForeignKey(Sala)
+	numero = models.IntegerField()
+
+class Funcion(models.Model):
+	idFuncion = models.AutoField(primary_key=True)
+	idPelicula = models.ForeignKey(Pelicula)
+	idAsiento = models.ForeignKey(Asiento)
+	hora_inicio = models.DateTimeField()
+
+class Reserva(models.Model):
+	idReserva = models.AutoField(primary_key=True)
+	idUsuario_asiste = models.ForeignKey(User, related_name="reserva_asiste")
+	idUsuario_asigna = models.ForeignKey(User, related_name="reserva_asigna")
+	idFuncion = models.ForeignKey(Funcion)
+	idAsiento = models.ForeignKey(Asiento)
